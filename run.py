14c886e5adca9eec204de02c17127505ba4a1609
yikiialvinlincoln <yikiialvinlincoln@gmail.com>
2024-04-21 14:44:20 +0300

    import function

---
+++ run.py
@@ -0,0 +1,3 @@
+import uplift
+print("this is the result from a function of a module",uplift.add1(20, 17))
+uplift.add1(51, 39)
---
+++ uplift.py
@@ -0,0 +1,14 @@
+def add1(a, b):
+    sum = a + b
+    print(sum)
+    return sum
+
+
+def sub1(a, b):
+    sub = a - b
+    print(sub)
+    return sub
+
+
+
+#create a package called abx and in that package create modules, addition, subtraction, multiplication and modulo and in each create a dynamic function that serves the purpose
